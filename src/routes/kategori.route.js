4be3233e78ecac026aada039d4f5dc006ad22b5c
adamrayw <raywibowo68@gmail.com>
2023-03-27 23:41:01 +0700

    remove multer, remove unused route, add edit kategori route

--- src/routes/kategori.route.js
+++ src/routes/kategori.route.js
@@ -1,19 +1,12 @@
 const express = require('express');
-const Multer = require('multer');
 const router = express.Router()
 const kategoriController = require('../controllers/kategori.controller')
-
-const storage = new Multer.memoryStorage()
-const upload = Multer({
-    storage,
-})
 
 // Get Menus
 router.get('/', kategoriController.getKategori)
 router.post('/add', kategoriController.addKategori)
 router.delete('/remove', kategoriController.remove)
 router.get('/getbyid/:id', kategoriController.getById)
-// router.post('/add', upload.single("image"), menuController.addItem)
-// router.put('/edit', upload.single("image"), menuController.edit)
+router.put('/edit', kategoriController.edit)
 
 module.exports = router
