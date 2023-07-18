3367dbaa9521cf0bd8254773af35b1ffdb857985
adamrayw <raywibowo68@gmail.com>
2023-07-19 02:22:08 +0700

    using multer

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router()
+const Multer = require('multer');
 const userController = require('../controllers/user.controller');
 const authenticateToken = require('../middlewares/authentication.middleware');
+
+const storage = new Multer.memoryStorage()
+const upload = Multer({
+    storage,
+})
+
 
 router.get('/dashboard/:id', authenticateToken, userController.getDashboard)
 router.get('/checkSubscription/:id', authenticateToken, userController.checkSubscription)
 router.get('/:id', authenticateToken, userController.getUserData)
-router.post('/update/:id', authenticateToken, userController.updateUserData)
+router.post('/update/:id', authenticateToken, upload.single("file"), userController.updateUserData)
 
 module.exports = router
